c0a40d74ef8c3d3978d1122a1f067731424dd942
iamMHasan <abcimhasanbro@gmail.com>
2022-11-24 18:26:57 +0600

    product details card added

--- src/pages/Home/ProductDetailsCard.js
+++ src/pages/Home/ProductDetailsCard.js
@@ -1,20 +1,46 @@
 import React from 'react';
+import { HiOutlineLocationMarker, HiUser } from "react-icons/hi";
 
 const ProductDetailsCard = ({ productDetails }) => {
     console.log(productDetails);
     const { image, location, name, resalePrice, originalPrice, sellerName, yearUse, datePosted } = productDetails
     return (
-        <div className='md:flex items-center justify-center gap-3'>
-            <div>
-                <img src={image} className='h-96 w-96' alt="" />
-            </div>
-            <div>
-                <div className="flex">
-                <h1 className="text-xl font-semibold">Seller Name : {sellerName}</h1>
-                <h1 className="text-xs font-semibold">Seller Location : {location}</h1>
+        <>
+            <div className='p-3 md:p-0 md:flex items-center justify-center gap-3 border-y-2 my-3'>
+                <div>
+                    <img src={image} className='h-96 w-96' alt="" />
+                </div>
+                <div className="flex flex-col justify-start">
+                    <div>
+                        <h1 className="text-xs">Product :<span className='text-xl text-green-700'>{name}</span> </h1>
+                        <hr className='my-3' />
+                        <div className="flex items-center justify-center gap-3 ">
+                            <div className="">
+                                <h1 className="text-xs flex items-center justify-center gap-1"><HiUser className='text-xl' /> <span className='text-xl text-green-700'>{sellerName}</span> </h1>
+                            </div>
+                            <hr className='my-3' />
+                            <div>
+                                <h1 className="text-xs flex items-center justify-center gap-1 "> <HiOutlineLocationMarker className='text-xl' /> <span className='text-xl text-green-700'>{location}</span></h1>
+                            </div>
+                        </div>
+                        <hr className='my-3' />
+                    </div>
+                    <div className='flex items-center  gap-3'>
+                        <p className="text-xs">Original Price : <span className='text-xl text-green-700'>{originalPrice}</span></p>
+
+                        <p className="text-xs">Resell Price :<span className='text-xl text-green-700'>{resalePrice}</span> </p>
+
+                    </div>
+                    <hr className='my-3' />
+                    <div className='flex items-center  gap-3'>
+                        <p className="text-xs">Years used :<span className='text-xl text-green-700'>{yearUse}</span></p>
+
+                        <p className="text-xs">Date posted : <span className='text-xl text-green-700'>{datePosted}</span></p>
+
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
