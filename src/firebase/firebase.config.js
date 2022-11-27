5362eafe2f4c2ec104dc233b7434a5b674bd6386
iamMHasan <abcimhasanbro@gmail.com>
2022-11-27 21:55:15 +0600

    added env file

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,14 +5,20 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAJFjKDoFjulWogq8IRDWffgUHKsnnsAyM",
-  authDomain: "kinobeco.firebaseapp.com",
-  projectId: "kinobeco",
-  storageBucket: "kinobeco.appspot.com",
-  messagingSenderId: "278080528059",
-  appId: "1:278080528059:web:2984412025be5e4572939e"
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId,
 };
-console.log(process.env.REACT_APP_apiKey);
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app;
+
+// apiKey: "AIzaSyAJFjKDoFjulWogq8IRDWffgUHKsnnsAyM",
+// authDomain: "kinobeco.firebaseapp.com",
+// projectId: "kinobeco",
+// storageBucket: "kinobeco.appspot.com",
+// messagingSenderId: "278080528059",
+// appId: "1:278080528059:web:2984412025be5e4572939e",
